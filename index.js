f32a731a28fc25302be945a949aa33fbc3358378
OmimaShalaby <omimashalaby@gmail.com>
2026-06-09 16:49:10 +0300

    fix bugs / update features

--- index.js
+++ index.js
@@ -32,20 +32,10 @@ app.use("/categories", categoryRoutes);
 app.use("/recipes", recipeRoutes);
 app.use("/favorites", favoriteRoutes);
 
-// app.get("/push", async(req, res)=>{
-//     const {data} = await axios.get("https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza");    
-//     const finalData = data.data.recipes;
-//     const recipes = finalData.map((recipe)=>({
-//         title: recipe.title,
-//         description: recipe.publisher,
-//         price: Math.floor(Math.random() * 10000000),
-//         image: recipe.image_url,
-//         createdBy: "6a2051e3afd5f4785369380c",
-//         categoryId: "6a107da5077e8730f1c8de4f"
-//     }));
-//     const result = await Recipe.insertMany(recipes);
-//     res.json(result);
-// });
+app.use("/", (req, res)=>{
+    res.json({msg:"Hello world"});
+})
+
 
 app.use(globalHandle);
 
